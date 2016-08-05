a06e3cb6c978037d3528a4fd84e2500c2de5fd43
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-08-04 23:38:22 -0700

    bug fixes + print svg

--- multidim.py
+++ multidim.py
@@ -1,14 +1,17 @@
 """Implements muli-dimensional threshold discovery via binary search."""
 from collections import namedtuple, deque
 from itertools import chain
+from pathlib import Path
 
 import svgwrite
 from numpy import array
+import numpy as np
+import funcy as fn
 
 Rec = namedtuple("Rec", "bot top")
 
 
-def binsearch(r: Rec, is_member, eps=0.1) -> (array, array, array):
+def binsearch(r: Rec, is_member, eps=0.001) -> (array, array, array):
     """Binary search over the diagonal of the rectangle.
     
     Returns the lower and upper approximation on the diagonal.
@@ -21,7 +24,7 @@ def binsearch(r: Rec, is_member, eps=0.1) -> (array, array, array):
         if not is_member(f(mid)):
             lo, hi = mid, hi
         else:
-            hi, lo = lo, mid
+            lo, hi = lo, mid
     return f(lo), f(mid), f(hi)
 
 
@@ -37,21 +40,21 @@ def backward_cone(p: array, r: Rec) -> Rec:
 
 def incomparable(p: array, r: Rec) -> [Rec]:
     """Computes the set of incomparable cones of point p."""
-    r01 = Rec(array(r.bot[0], p[1]), array(p[0], r.top[1]))
-    r10 = Rec(array(p[0], r.bot[1]), array(r.top[0], p[1]))
+    r01 = Rec(array([r.bot[0], p[1]]), array([p[0], r.top[1]]))
+    r10 = Rec(array([p[0], r.bot[1]]), array([r.top[0], p[1]]))
     return [r01, r10]
 
 
-def multidim_search(rec: Rec, is_member) -> [(set(Rec), set(Rec))]:
+def multidim_search(rec: Rec, is_member) -> [({Rec}, {Rec})]:
     """Generator for iteratively approximating the oracle's threshold."""
     queue = deque([rec])
-    good_approx, bad_approx = set(), set()
+    good_approx, bad_approx = [], []
     while True:
         rec = queue.pop()
         low, mid, high = binsearch(rec, is_member)
 
-        bad_approx.add(backward_cone(low, rec))
-        good_approx.add(forward_cone(high, rec))
+        bad_approx.append(backward_cone(low, rec))
+        good_approx.append(forward_cone(mid, rec))
         queue.extendleft(incomparable(mid, rec))
 
         yield bad_approx, good_approx
@@ -59,17 +62,46 @@ def multidim_search(rec: Rec, is_member) -> [(set(Rec), set(Rec))]:
 
 def draw_rec(dwg, r:Rec, is_member:bool):
     """TODO: handle different orientations."""
-    bot = tuple(map(int, r.bot))
-    dim = tuple(map(int, r.top - r.bot))
+    bot = tuple(map(float, r.bot))
+    dim = tuple(map(float, r.top - r.bot))
     color = "red" if is_member else "green"
     return dwg.rect(bot, dim, fill=color)
 
 
-def draw_domain(dwg, r:Rec, good:set(Rec), bad:set(Rec)):
-    width, height = tuple(map(int, r.top - r.bot))
+def draw_domain(r:Rec, good:{Rec}, bad:{Rec}, scale):
+    width, height = tuple(map(float, scale(r.top - r.bot)))
     dwg = svgwrite.Drawing(width=width, height=height)
-    good_recs = (draw_rec(dwg, r, True) for r in good)
-    bad_recs = (draw_rec(dwg, r, False) for r in bad)
+    scale_rec = lambda x: Rec(scale(x.bot), scale(x.top))
+    good_recs = (draw_rec(dwg, scale_rec(r), True) for r in good)
+    bad_recs = (draw_rec(dwg, scale_rec(r), False) for r in bad)
+
     for svg_rec in chain(good_recs, bad_recs):
         dwg.add(svg_rec)
     return dwg
+
+
+def multidim_search_and_draw(rec, is_member, n, save_path):
+    bad, good = fn.nth(n, multidim_search(rec, is_member))
+    # TODO automate detecting a good scale
+    # currently assumes -1, 1 to 0, 100 transformation
+    scale = lambda x: 100*(x +1)
+    dwg = draw_domain(rec, good=good, bad=bad, scale=scale)
+    with Path(save_path).open('w') as f:
+        dwg.write(f)
+
+
+def main():
+    R = Rec(-np.ones(2), np.ones(2))
+    n = np.array([1,1])/np.sqrt(2)
+    f = lambda x: x@n > 0
+    multidim_search_and_draw(R, f, 100, "foo.svg")
+
+    f = lambda x: x[0] > 0
+    multidim_search_and_draw(R, f, 100, "foo2.svg")
+
+    f = lambda x: x[1] > 0
+    multidim_search_and_draw(R, f, 100, "foo3.svg")
+
+if __name__ == "__main__":
+    main()
+    
