fix precision errors/polarity errors in weightedbinsearch

multidim_threshold/learn.py
@@ -2,10 +2,12 @@
 from collections import namedtuple
 from itertools import combinations
 from heapq import heappush as hpush, heappop as hpop
+from math import isclose
 
 import numpy as np
 from numpy import array
 import funcy as fn
+
 
 Rec = namedtuple("Rec", "bot top")
 Result = namedtuple("Result", "vol mids unexplored")
@@ -31,7 +33,6 @@ def binsearch(r:Rec, stleval, eps=1e-3):
         mid = lo + (hi - lo) / 2
         lo, hi = (mid, hi) if feval(mid) ^ polarity else (lo, mid)
 
-    # Want satisifiable formula
     return f(lo), f(mid), f(hi)
 
 
@@ -40,28 +41,31 @@ def weightedbinsearch(r: Rec, robust, eps=0.01) -> (array, array, array):
     diag = r.top - r.bot
     f = lambda t: r.bot + t * diag
     frobust = lambda t: robust(f(t))
-
     # They are opposite signed
-    rh, rl = frobust(hi), frobust(lo)
-    if rh * rl >= 0:
+    frhi, frlo = frobust(hi), frobust(lo)
+    polarity = np.sign(frlo)
+
+    # Early termination via bounds checks
+    if frhi * frlo >= 0:
         flo, fhi = f(lo), f(hi)
-        fmid = flo if rh < 0 else fhi
+        fmid = flo if frhi < 0 else fhi
         return flo, fmid, fhi
-    else:
-        while hi - lo > eps:
-            frlo, frhi = frobust(lo), frobust(hi)
-            ratio = frlo / (frhi - frlo)
-            mid = lo - (hi - lo)*ratio
-            frmid = frobust(mid)
-            # hi and robustness 
-            # of mid are opposite signed
-            if frmid * frhi < 0:
-                lo, hi = mid, hi
-            elif frmid * frlo < 0:
-                lo, hi = lo, mid
-            else:
-                lo, hi = mid - eps/2, mid + eps/2
-                break
+
+    while hi - lo > eps:
+        ratio = frlo / (frhi - frlo)
+        mid = lo - (hi - lo)*ratio
+        frmid = frobust(mid)
+
+        # Check if we've almost crossed the boundary
+        # Note: Because diag is opposite direction of
+        # the boundary, the crossing point is unique.
+        if isclose(frmid, 0, abs_tol=eps):
+            lo, hi = mid - eps/2, mid + eps/2
+            break
+
+        lo, hi = (mid, hi) if frmid * frhi < 0 else (lo, mid)
+        frlo, frhi = frobust(lo), frobust(hi)
+
 
     return f(lo), f(mid), f(hi)
 
@@ -75,7 +79,8 @@ def gridSearch(r: Rec, is_member, eps=0.1):
     while queue:
         node, prev = hpop(queue)
         if not(is_member(node) ^ polarity):
-            mids.add(tuple(list(prev)))
+            mid = eps/2*(prev-node) + node
+            mids.add(tuple(list(mid)))
         else:
             for c in children(node):
                 hpush(queue, (c, node))

multidim_threshold/test_learn.py
@@ -29,7 +29,7 @@ b2 = mdt.Rec(np.array([-2, -1, -2]), np.array([0, 0, 0]))
 n2 = np.array([1, 1, 1]) / np.sqrt(3)
 F2 = lambda x: x @n2 > 0
 
-ex1d = (lambda p: p > 5, lambda p: p - 5, 
+ex1d = (lambda p: p > 5.435, lambda p: p - 5.435, 
         mdt.Rec(np.array([-9]), np.array([9])))
 
 
@@ -64,10 +64,10 @@ class TestMultidimSearch(unittest.TestCase):
     @params(ex1d)
     def test_equiv_1d_mids(self, f, r, rec):
         _, mid1, _ = mdt.binsearch(rec, f, eps=0.0001)
-        _, mid2, _ = mdt.weightedbinsearch(rec, r, eps=0.01)
-        res = mdt.gridSearch(rec, f, eps=0.01)
-        self.assertAlmostEqual(float(mid1 - mid2), 0, places=3)
-        self.assertAlmostEqual(float(res.mids[0] - mid2), 0, places=3)
+        _, mid2, _ = mdt.weightedbinsearch(rec, r, eps=0.0001)
+        self.assertAlmostEqual(float(mid1), float(mid2), places=2)
+        res = mdt.gridSearch(rec, f, eps=0.0001)
+        self.assertAlmostEqual(float(res.mids[0] ), float(mid2), places=2)
 
 
     @params(ex1d)
@@ -82,9 +82,8 @@ class TestMultidimSearch(unittest.TestCase):
         _, mid1, _ = mdt.weightedbinsearch(rec, r, eps=0.0001)
         _, mid2, _ = mdt.weightedbinsearch(rec, neg_r, eps=0.0001)
         self.assertAlmostEqual(float(mid1 - mid2), 0, places=3)
+        
 
         mid1 = float(mdt.gridSearch(rec, f, eps=0.01).mids[0])
         mid2 = float(mdt.gridSearch(rec, neg_f, eps=0.01).mids[0])
         self.assertAlmostEqual(float(mid1 - mid2), 0, places=3)
-
-