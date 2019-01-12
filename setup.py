39c821b3ce01a01080f84ad8f839f9c4f4a7528d
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-11 21:47:36 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import find_packages, setup
 
-setup(name='monotone-bipartition',
-      version='0.0.0',
-      description='TODO',
-      url='http://github.com/mvcisback/monotone-bipartition',
-      author='Marcell Vazquez-Chanlatte',
-      author_email='marcell.vc@eecs.berkeley.edu',
-      license='MIT',
-      install_requires=[
-          'lenses',
-          'funcy',
-      ],
-      packages=find_packages(),
+
+setup(
+    name='monotone-bipartition',
+    version='0.0.0',
+    description='TODO',
+    url='http://github.com/mvcisback/monotone-bipartition',
+    author='Marcell Vazquez-Chanlatte',
+    author_email='marcell.vc@eecs.berkeley.edu',
+    license='MIT',
+    install_requires=[
+        'lenses',
+        'funcy',
+    ],
+    packages=find_packages(),
 )
