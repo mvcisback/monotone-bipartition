fa6c1f1302fb13b85babc281da17b67a21fcdffd
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-13 23:52:29 -0800

    implement labeling using lazytree

--- monotone_bipartition/bipartition.py
+++ monotone_bipartition/bipartition.py
@@ -12,11 +12,31 @@ from monotone_bipartition import search as mdts  # binsearch
 class BiPartition:
     tree: LazyTree = attr.ib()
 
+    @property
+    def dim(self):
+        return len(self.tree.view())
+
+    @property
+    def domain(self):
+        return self.tree.view()
+    
     def dist(self, other) -> float:
         raise NotImplementedError
 
     def label(self, point) -> bool:
-        raise NotImplementedError
+        # TODO: Should support either finite precision or max depth.
+        domain = self.domain
+
+        def not_comparable(rec, point):
+            return point not in domain.forward_cone(rec.bot) and \
+                point not in domain.backward_cone(rec.top)
+
+        recs = self.tree.prune(not_comparable).leaves()
+        for rec in recs:
+            if rec in domain.forward_cone(rec.top):
+                return True
+            elif rec in domain.backward_cone(rec.bot):
+                return False
 
 
 def from_threshold(func, dim: int) -> BiPartition:
