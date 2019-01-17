3c15c9b8b8a06119f926cc342663c196d799d729
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-16 21:54:34 -0800

    Untested adaptive hausdorff distance. First draft

--- monotone_bipartition/hausdorff.py
+++ monotone_bipartition/hausdorff.py
@@ -6,6 +6,10 @@ import funcy as fn
 from monotone_bipartition import rectangles as mdtr  # Interval
 
 
+def dinf(a, b):
+    return np.linalg.norm(np.array(a) - np.array(b), ord=float('inf'))
+
+
 def pointwise_hausdorff(xs, ys):
     def d(a, b):
         return np.linalg.norm(np.array(a) - np.array(b), ord=float('inf'))
@@ -33,20 +37,24 @@ def discretized_and_pointwise_hausdorff(recset1, recset2, k=3):
 
 def node_dist(n1, n2) -> mdtr.Interval:
     r1, r2 = n1.view(), n2.view()
-    
-    raise NotImplementedError
+    points1 = r1.corners | {r1.center}
+    points2 = r2.corners | {r2.center}
+    dists = [dinf(p1, p2) for p1, p2 in product(points1, points2)]
+    return mdtr.Interval(min(dists), max(dists))
 
 
-def worst_case(n1, dists):
-    pass
+def worst_case(node1, approx2, adj_mat):
+    response = min(approx2, key=lambda node2: adj_mat[node1, node2])
+    return (node1, node2), adj_mat[node1, response]
 
 
-def best_worst_case(dists):
-    pass
+def best_worst_case(approx1, approx2, adj_mat):
+    move2score = dict(worst_case(n, approx2, adj_mat) for n in approx1)
+    return max(move2score.keys(), key=move2score.get)
 
 
-def gen_dists(part1, part2):
-    """generator for directed hausdorff distances"""
+def gen_dists(part1, part2, *, prune=True):
+    """Generator for directed hausdorff distances."""
     approx1, approx2 = {part1.tree}, {part2.tree}
     new1, new2 = approx1, approx2
     dists = {}
@@ -56,14 +64,15 @@ def gen_dists(part1, part2):
         dists.extend({(n1, n2): node_dist(n1, n2) for n1, n2 in comparisons})
         n1, n2 = best_worst_case(dists)
         imin = dists[n1, n2]
-        yield imin
+        yield imin   # Best score must lie in the interval.
 
-        # Prune intervals that can't contribute to min dist.
-        utils = fn.select_values(
-            lambda i: i.bot < imin.top,
-            utils
-        )
-        approx1, approx2 = map(set, zip(*dists.keys()))
+        if prune:
+            # Prune intervals that can't contribute to min dist.
+            utils = fn.select_values(
+                lambda i: (i & imin) is not None,
+                utils
+            )
+            approx1, approx2 = map(set, zip(*dists.keys()))
 
         # Refine.
         new1, new2 = set(n1.children), set(n2.children)
