1d4579b7905f7f91f34fd1236f62bb9878764e8e
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-19 15:32:13 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='monotone-bipartition',
-    version='0.1.1',
+    version='0.1.2',
     description='Compute Monotone Threshold Surfaces and'
     'compute distances between surfaces.',
     url='http://github.com/mvcisback/monotone-bipartition',
